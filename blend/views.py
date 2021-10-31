601666f5be5e7bdd8aa70364392d515548bc616c
metalcode03 <49570754+metalcode03@users.noreply.github.com>
2021-10-31 06:04:00 +0100

    exporting as csv and excel views

--- blend/views.py
+++ blend/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .forms import UploadForms
 from .models import UploadAndBlendImg
+import datetime
+import csv
+import xlwt
 
 # Create your views here.
 def index(request):
@@ -21,4 +25,46 @@ def upload(request, name):
         "upload": uploaded
     }
     return render(request, 'test.html', context)
+
+def export_csv(request):
+    response = HttpResponse(content_type='text/csv')
+    response['Content-Desposition'] = 'attachment; filename=Expenses' + str(datetime.datetime.now()) + '.csv'
     
+    writer = csv.writer(response)
+    row = ["name", "file_upload", "file_genrated"]
+    writer.writerow(row)
+    
+    uploads = UploadAndBlendImg.objects.all()
+    
+    for upload in uploads:
+        writer.writerow([upload.name, upload.fileUpload, upload.bgUpload])
+    
+    return response
+
+def export_excel(request):
+    response = HttpResponse(content_type='application/ms-excel')
+    response['Content-Desposition'] = 'attachment; filename=Expenses' + str(datetime.datetime.now()) + '.xls'
+    
+    wb = xlwt.Workbook(encoding='utf-8')
+    ws = wb.add_sheet('Expenses')
+    row_num = 0
+    font_style = xlwt.XFStyle()
+    font_style.font.bold = True
+    column = ["name", "file_upload", "file_genrated"]
+    
+    for col_num in range(len(column)):
+        ws.write(row_num, col_num, column[col_num], font_style)
+        
+    font_style = xlwt.XFStyle()
+    
+    rows = UploadAndBlendImg.objects.all().values_list(
+        "name", "fileUpload", "bgUpload")
+    
+    for row in rows:
+        row_num += 1
+        
+        for col_num in range(len(row)):
+            ws.write(row_num, col_num, str(row[col_num]), font_style)
+            
+    wb.save(response)
+    return response
